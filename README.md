41baf0a43912f40d6e06f06b27792cb10fcd86ea
Singgih Pratama <singgih.vendor.pasti@pegadaian.co.id>
2023-10-29 20:36:48 +0700

    membuat httpserver dengan hapi

--- README.md
+++ README.md
@@ -1,2 +1,16 @@
 # hapi-webserver
 Membangun Web Server menggunakan Framework Hapi
+default response:
+{
+"statusCode": 404,
+"error": "Not Found",
+"message": "Not Found"
+}
+
+<!-- cek koneksi menggunakan terminal -->
+curl -X POST http://127.0.0.1:5000/ -i
+
+Step for using Hapi:
+
+1. npm install @hapi/hapi
+2. request handler dituliskan di dalam route configuration? Yap benar! Handler pada Hapi dipisahkan berdasarkan route yang ada. Setiap spesifikasi route memiliki handler-nya masing-masing. Dengan begitu, tentu kode akan lebih mudah dikelola. Anda bisa mengatakan selamat tinggal pada if else yang bersarang.
--- server.js
+++ server.js
@@ -1,1 +1,27 @@
-console.log("I'll make server with Hapi")
+const Hapi = require('@hapi/hapi');
+
+const init = async () => {
+    const server = Hapi.Server({
+        port: 5000,
+        host: '127.0.0.1',
+    });
+
+    server.route({
+        method: "GET",
+        path: "/",
+        handler: (requst, h) => {
+            return "Hello World Hapi!";
+        }
+    })
+
+    await server.start();
+    // console.log(`Server berjalan pada ${server.info.uri}`);
+    console.log("Server running on %s", server.info.uri);
+};
+
+process.on("unhandledRejection", (err) => {
+    console.log(err);
+    process.exit(1);
+});
+
+init();
